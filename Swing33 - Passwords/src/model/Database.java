73c9f180311501280edbf65fa1e34f8ac504420d
Pedro Gonzalez Ferrandez <pdro74@hotmail.com>
2015-06-25 09:47:13 +0200

    update

---
+++ Swing33 - Passwords/src/model/Database.java
@@ -0,0 +1,67 @@
+package model;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Database {
+	
+	private List<Person> people;
+
+	public Database() {
+		/* linked lists are optimized to add and delete items
+		 * anywhere in the list
+		 */
+		people = new LinkedList<Person>();
+	}
+	
+	public void addPerson(Person person) {
+		people.add(person);
+	}
+	
+	public List<Person> getPeople() {
+		return Collections.unmodifiableList(people);
+	}
+	
+	public void saveToFile(File file) throws IOException {
+		FileOutputStream fos = new FileOutputStream(file);
+		ObjectOutputStream oos = new ObjectOutputStream(fos);
+		
+		Person[] persons = people.toArray(new Person[people.size()]);
+		
+		oos.writeObject(persons);
+		
+		oos.close();
+	}
+	
+	public void loadFromFile(File file) throws IOException {
+		FileInputStream fis = new FileInputStream(file);
+		ObjectInputStream ois = new ObjectInputStream(fis);
+		
+		try {
+			Person[] persons = (Person[]) ois.readObject();
+			
+			people.clear();
+			
+			people.addAll(Arrays.asList(persons));
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		ois.close();
+	}
+
+	public void removePerson(int index) {
+		people.remove(index);
+	}
+	
+}
