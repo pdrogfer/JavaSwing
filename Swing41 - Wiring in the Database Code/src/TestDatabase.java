8a908929faf0d9f772c3999aec053ad0df5c9fdc
peders2 <pedrogf527@gmail.com>
2015-07-05 11:54:30 +0200

    Swing 41 done, wiring app and database

---
+++ Swing41 - Wiring in the Database Code/src/TestDatabase.java
@@ -0,0 +1,40 @@
+import java.sql.SQLException;
+
+import model.AgeCategory;
+import model.Database;
+import model.EmploymentCategory;
+import model.Gender;
+import model.Person;
+
+public class TestDatabase {
+
+	public static void main(String[] args) {
+		System.out.println("Running database test");
+		
+		Database db = new Database();
+		try {
+			db.connect();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		db.addPerson(new Person("Robert Redford", AgeCategory.adult, EmploymentCategory.unemployed, "44", true, Gender.male, "actor"));
+		db.addPerson(new Person("John Travolta", AgeCategory.senior, EmploymentCategory.unemployed, "123", false, Gender.female, "actress"));
+		
+		try {
+			db.save();
+			System.out.println("saved to database");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			db.load();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		db.disconnect();
+	}
+
+}
