598d769f399bbfd916dc409667d75728fb011cbf
peders2 <pedrogf527@gmail.com>
2015-08-18 08:58:41 +0200

    added up to swing57 Progress Bars

---
+++ Swing56 - Modal Dialogs/src/gui/Utils.java
@@ -0,0 +1,33 @@
+package gui;
+
+import java.net.URL;
+
+import javax.swing.ImageIcon;
+
+public class Utils {
+
+	public static String getFileExtension(String name) {
+		
+		int pointIndex = name.lastIndexOf(".");
+		
+		if (pointIndex == -1) {
+			return null;
+		} if (pointIndex == name.length() - 1) {
+			return null;
+		}
+		
+		return name.substring(pointIndex + 1, name.length());
+	}
+	
+	public static ImageIcon createIcon(String path) {
+		URL url = System.class.getResource(path);
+		
+		if (url == null) {
+			System.err.println("Unable to load icon image:" + path);
+		}
+		
+		ImageIcon icon = new ImageIcon(url, "Icon for the button");
+		
+		return icon;
+	}
+}
